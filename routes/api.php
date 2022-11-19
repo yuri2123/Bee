1db2f8aca0c83e81663a17925c7791accc47b1db
yuri2123 <yuri.lucio@misena.edu.co>
2022-11-19 16:07:54 -0500

    arreglos

--- routes/api.php
+++ routes/api.php
@@ -20,12 +20,12 @@ use Illuminate\Support\Facades\Route;
 */
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
-Route::get('/users',[AuthController::class,'index']);
+
 
 Route::group(['middleware' => ["auth:sanctum"]], function(){
     Route::post('logout', [AuthController::class,'logout']);
     Route::get('user_profile', [AuthController::class,'userProfile']);
-    
+    Route::get('/users',[AuthController::class,'index']);
 
 });
 
