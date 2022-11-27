3c9c41c93c9f325d8b766664554c8ff780eb0982
yuri2123 <yuri.lucio@misena.edu.co>
2022-11-26 19:59:00 -0500

    Arreglo

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -15,8 +15,8 @@ class AuthController extends Controller
 
        //////  LISTA TODOS LOS USUARIOS    ///////
 
-    public function index($id){
-        $users = User::find($id);
+    public function index(){
+        $users = User::all();
         return response()->json([
           'message' => 'success',
           'info' => 'Lista de usuarios',
